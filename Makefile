c8edad8c1490f5b9f995ece744f76a2a3b34a9ce
Seth Heeren <sgheeren@gmail.com>
2013-06-12 14:39:30 +0200

    q17064905

--- Makefile
+++ Makefile
@@ -4,17 +4,11 @@ CPPFLAGS+=-std=c++0x
 CPPFLAGS+=-g -O0 -Wall
 CPPFLAGS+=-isystem ~/custom/boost/
 
-# CPPFLAGS+=-fopenmp
-# CPPFLAGS+=-march=native
-
-LDFLAGS+=-L ~/custom/boost/stage/lib/
-LDFLAGS+=-lboost_system -lboost_regex -lboost_thread -lpthread
-
-CXX=clang++
-CC=clang
-
-CXX=/usr/lib/gcc-snapshot/bin/g++
-CC=/usr/lib/gcc-snapshot/bin/gcc
+# CXX=clang++
+# CC=clang
+# 
+# CXX=/usr/lib/gcc-snapshot/bin/g++
+# CC=/usr/lib/gcc-snapshot/bin/gcc
 
 test.o JSON.o: JSON.hpp
 
--- test.cpp
+++ test.cpp
@@ -1,62 +1,29 @@
 #include <sstream>
 #include "JSON.hpp"
 
-// util
-static JSON::Value roundtrip(JSON::Value const& given) {
-    return JSON::parse(to_wstring(given));
-}
-
-void roundtrip_test()
-{
-    auto 
-        document = JSON::readFrom(std::istringstream(
-                    "{\r\n"
-                    "       \"Event\": {\r\n"
-                    "             \"attribute_a\": 0.0002,\r\n"
-                    "\"attribute_b\": 2e-005,\r\n"
-                    "\"attribute_c\": 0.022\r\n"
-                    "}\r\n}")),
-        verify = roundtrip(document);
-
-    std::cout << verify;
-    std::cout << "document <=> verify equal:     \t" << std::boolalpha << (document == verify)                       << "\n";
-    std::cout << "document <=> verify text match:\t" << std::boolalpha << (to_string(document) == to_string(verify)) << "\n";
-}
-
-void initializer_test()
-{
-    using namespace JSON;
-
-    const Array arr { 
-        L"text", 
-        42,
-        Object { { L"dummy", Null() } } 
-    };
-
-    auto radius = as_double(arr[1]);
-
-    auto const document = Object {
-            { L"number", 314e-2 },
-            { L"string", L"hello\ngoodbye" },
-            { L"array" , arr },
-            { L"bool" , False() },
-            { L"radius", radius },
-            { L"area", radius * radius * 3.14 },
-            { String { 10, L'=' }, String { 10, L'*' } }
-    };
-
-    std::cout << document[L"bool"]   << std::endl;
-    std::cout << document[L"number"] << std::endl;
-    std::cout << document[L"string"] << std::endl;
-    std::cout << document[L"array"]  << std::endl;
-    std::cout << document[L"bool"]   << std::endl;
-    std::cout << document[L"radius"] << std::endl;
-    std::cout << document[L"area"]   << std::endl;
-    std::cout << document            << std::endl;
-}
-
 int main()
 {
-    roundtrip_test();
-    initializer_test();
+    auto document = JSON::readFrom(std::istringstream(
+                "{\"RootA\":\"Value in parent node\",\"ChildNode\":{\"ChildA\":[1,2],\"ChildB\":42}}"));
+
+    auto childA = as_object(
+                as_object(document)[L"ChildNode"]
+            )[L"ChildA"];
+
+    std::cout << childA << std::endl;
+
+    // or use each value
+    for(auto& value : as_array(childA).values)
+        std::cout << value << std::endl;
+
+    // more advanced:
+    JSON::Value expected = JSON::Object {
+        { L"RootA", L"Value in parent node" },
+        { L"ChildNode", JSON::Object {
+                 { L"ChildA", JSON::Array { 1,2 } },
+                 { L"ChildB", 42 },
+             } },
+    };
+    std::cout << "Check equality: " << std::boolalpha << (document == expected) << std::endl;
+    std::cout << "Serialized: " << document << std::endl;
 }
