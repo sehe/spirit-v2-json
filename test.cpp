fb73fbc0ca5b67824f86ed2e8aa48d9434c832ac
Seth Heeren <sgheeren@gmail.com>
2013-07-18 02:22:47 +0200

    simplified and fixed for assymetric leaf values. 10 lines shorter

--- test.cpp
+++ test.cpp
@@ -2,44 +2,31 @@
 
 namespace {
     using namespace JSON;
-    struct make_love : boost::static_visitor<void>
+    struct make_love : boost::static_visitor<bool>
     {
-        bool top_level;
-        make_love(bool top_level = true) : top_level(top_level) {}
-
-        void operator()(Object& a, Object const& b) const
-        {
-            for(auto el: b.values)
-            {
-                if (a.values.end() != a.values.find(el.first))
-                {
-                    boost::apply_visitor(make_love(false), a[el.first], el.second);
-                } else
-                {
-                    a.values[el.first] = el.second;
-                }
-            }
+        bool operator()(Object& a, Object const& b) const {
+            for(auto el: b.values) 
+                recurse(a[el.first], el.second);
+            return true;
         }
+        template<typename T, typename U> bool operator()(T& a, U const& b)  const 
+            { return false; }
 
-        template<typename T> void operator()(T& a, T const& b)  const
-            {
-                if (!top_level)
-                    a = b;
-            }
-
-        template<typename T, typename U> void operator()(T& a, U const& b)  const
-            {
-                std::cerr << "Warning: incompatible arguments (" << typeid(T).name() << ", " << typeid(U).name() << ")\n";
-            }
+      private:
+        void recurse(Value& a, Value const& b) const {
+            if (!boost::apply_visitor(*this, a, b))
+                a = b;
+        }
     };
 }
 
 int main()
 {
     auto jsonA = JSON::parse("{ \"a\":\"1\", \"b\":\"2\", \"c\":{\"a\":\"1\", \"b\":\"2\"} }");
-    auto jsonB = JSON::parse("{ \"b\":\"2new\", \"c\":{\"a\":\"1new\"} }");
+    auto jsonB = JSON::parse("{ \"b\":42, \"c\":{\"a\":\"1new\"}, \"q\":[3.14,null] }");
 
-    boost::apply_visitor(make_love(), jsonA, jsonB);
-
-    std::cout << jsonA;
+    if (boost::apply_visitor(make_love(), jsonA, jsonB))
+        std::cout << "Merged: " << jsonA;
+    else
+        std::cerr << "Couldn't merge '" << jsonA << "' with '" << jsonB << "'\n";
 }
